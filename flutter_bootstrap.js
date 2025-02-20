0f8f5077ec71487268473627612d9df28fa82a5e
omar Abdlnaby <omarmacmini@omars-Mac-mini.local>
2025-02-20 22:12:57 +0200

    Fix 404 error on GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3197369032"
+    serviceWorkerVersion: "3490241698"
   }
 });
